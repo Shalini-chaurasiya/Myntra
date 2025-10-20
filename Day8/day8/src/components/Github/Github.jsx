6755b242566d80fb921454441e6c561a935accf2
Shalini-chaurasiya <shalinichaurasiya2203@gmail.com>
2025-10-20 16:02:35 +0530

    Initial commit

--- Day8/day8/src/components/Github/Github.jsx
+++ Day8/day8/src/components/Github/Github.jsx
@@ -15,7 +15,9 @@ function Github() {
     
   return (
     <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data.followers}
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followings: {data.following}
     <img src={data.avatar_url} alt="Git picture" width={300} />
+    </div>
     </div>
   )
 }
